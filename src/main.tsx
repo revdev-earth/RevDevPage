e60b3d77c9e3c26d2ade076ad2911a59fd836209
Nickothan <s1nicolasrg@gmail.com>
2022-02-14 09:48:04 -0500

    Add styled components, make and import to main.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+//* Import components
 import App from "./App"
+
+//* Import styles
+import { GlobalStyle } from "./config/globalStyles"
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
