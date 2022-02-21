74d6b49d32e520c8f75bd5cc29c2b7ebd7b3cd42
Nickothan <s1nicolasrg@gmail.com>
2022-02-21 08:01:45 -0500

    Layout first section from home.

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,3 +1,26 @@
+import ImgHome1 from "../../assets/home/img-home-1.png"
+
 export default function Home() {
-  return <div>Home</div>
+  return (
+    <main>
+      <div>
+        <div>
+          <h2>
+            We helped you create your <span>website</span>
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. At ut
+            turpis adipiscing tempus, magna elit nunc iaculis sit. Libero velit
+            quis leo non. At donec egestas cras in libero pellentesque. Donec
+            amet phasellus
+          </p>
+          <div>
+            <button>Get Started</button>
+            <button>contact Us</button>
+          </div>
+        </div>
+        <img src={ImgHome1} alt="" />
+      </div>
+    </main>
+  )
 }
