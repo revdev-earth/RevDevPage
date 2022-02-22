b1c7feb6c3e2b06761063f9e558989ee108d2aa5
Nickothan <s1nicolasrg@gmail.com>
2022-02-22 14:14:12 -0500

    update widths and responsive from firstSection.

--- src/Pages/Home/styles.ts
+++ src/Pages/Home/styles.ts
@@ -1,9 +1,10 @@
-import styled, { ThemeConsumer } from "styled-components"
+import styled from "styled-components"
 
 export const FirstSection = styled.section`
   margin: auto;
-  max-width: 1024px;
-  display: flex;
+  padding: 0 100px;
+  display: grid;
+  grid-template-columns: 3fr 4fr;
 
   div {
     margin: center;
@@ -13,18 +14,23 @@ export const FirstSection = styled.section`
     gap: 20px;
     justify-content: center;
 
-    h4 {
-      width: 350px;
+    h3 {
+      position: relative;
+      max-width: 500px;
       color: ${({ theme }) => theme.colors.primary};
 
-      &:after {
+      &:before {
+        content: "";
+        position: absolute;
+        top: -30px;
         width: 200px;
-        background-color: solid ${({ theme }) => theme.colors.secondary};
+        height: 5px;
+        background-color: ${({ theme }) => theme.colors.secondary};
       }
 
       span {
         color: ${({ theme }) => theme.colors.secondary};
-        font-size: ${({ theme }) => theme.fonts.h4};
+        font-size: ${({ theme }) => theme.fonts.h3};
       }
     }
 
@@ -36,6 +42,50 @@ export const FirstSection = styled.section`
   }
 
   img {
-    max-width: 500px;
+    padding-right: 50px;
+    max-width: 100%;
+    max-height: 750px;
+  }
+
+  @media (max-width: 1250px) {
+    padding: 50px 80px;
+
+    div {
+      padding-left: 10px;
+    }
+  }
+  @media (max-width: 1024px) {
+    padding: 50px;
+    grid-template-columns: 1fr 1fr;
+
+    div {
+      padding-left: 0px;
+
+      h3:before {
+        top: -10px;
+      }
+    }
+  }
+
+  @media (max-width: 999px) {
+    padding: 50px 30px;
+
+    grid-template-columns: 4fr 2fr;
+
+    img {
+      padding-right: 0;
+    }
+  }
+
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr 1fr;
+
+    div {
+      grid-row: 2;
+    }
+    img {
+      grid-row: 1;
+    }
   }
 `
