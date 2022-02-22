e31f287958657ad621e8e4067e4c8fc7fb6550ec
Nickothan <s1nicolasrg@gmail.com>
2022-02-22 15:25:10 -0500

    Update styles feom secondSection in home.

--- src/components/common/HomeServices/index.tsx
+++ src/components/common/HomeServices/index.tsx
@@ -1,12 +1,21 @@
+//*Import styles
+import { Container, Circle } from "./styles"
+
+//* Import assets
+import ArrowRoght from "../../../assets/arrow-right.svg"
+
 interface HomeServicesProps {
   item: { img: string; title: string; description: string; link: string }
 }
 
 export const HomeServices = ({ item }: HomeServicesProps) => (
-  <li>
-    <h4>{item.title}</h4>
+  <Container>
+    <Circle />
     <img src={item.img} alt={item.img} />
+    <h4>{item.title}</h4>
     <p>{item.description}</p>
-    <a href={item.link}>Read More</a>
-  </li>
+    <a href={item.link}>
+      Read More <img src={ArrowRoght} alt="arrow right" />
+    </a>
+  </Container>
 )
