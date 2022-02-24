16cfcb1f0b9640bda5ff366e3811c3a6d3e2bbb9
Nickothan <s1nicolasrg@gmail.com>
2022-02-24 10:16:21 -0500

    function pag from third section.

--- src/Pages/Home/ThirdSection/index.tsx
+++ src/Pages/Home/ThirdSection/index.tsx
@@ -1,51 +1,35 @@
+import { useEffect, useState } from "react"
+
 //* Import components
 import { Button } from "@components"
 
 //* Import styles
 import { Container } from "./styles"
 
-//* Import assets
-import Img1 from "@assets/home/Image-h-1.png"
-import Img2 from "@assets/home/Image-h-2.png"
-import Img3 from "@assets/home/Image-h-3.png"
+import { homeWorks } from "@data"
 
-export const ThirdSection = () => (
-  <Container>
-    <h4>Portfolio</h4>
-    <h3>Our Great Work</h3>
-    <div>
-      <h4>Website Optimization</h4>
+export const ThirdSection = () => {
+  const [pag, setPag] = useState(0)
 
-      <h4>Website Redesign</h4>
+  return (
+    <Container>
+      <h4>Portfolio</h4>
+      <h3>Our Great Work</h3>
+      <div>
+        <h4 onClick={() => setPag(0)}>Website Optimization</h4>
 
-      <h4>Search Engine Optimization</h4>
-    </div>
-    <ul>
-      <li>
-        <img src={Img1} />
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi cursus
-          vitae tortor sapien, lectus scelerisque porttitor. Dolor nulla
-          bibendum
-        </p>
-      </li>
-      <li>
-        <img src={Img2} />
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi cursus
-          vitae tortor sapien, lectus scelerisque porttitor. Dolor nulla
-          bibendum
-        </p>
-      </li>
-      <li>
-        <img src={Img3} />
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi cursus
-          vitae tortor sapien, lectus scelerisque porttitor. Dolor nulla
-          bibendum
-        </p>
-      </li>
-    </ul>
-    <Button primary>See All</Button>
-  </Container>
-)
+        <h4 onClick={() => setPag(1)}>Website Redesign</h4>
+
+        <h4 onClick={() => setPag(2)}>Search Engine Optimization</h4>
+      </div>
+      <ul>
+        {homeWorks[pag].map((item) => (
+          <li>
+            <img src={item.img} alt="img-item" /> <p>{item.description}</p>
+          </li>
+        ))}
+      </ul>
+      <Button primary>See All</Button>
+    </Container>
+  )
+}
