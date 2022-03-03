f76e1dd33b62e591579f4ac02a17312f3175b615
Nickothan <s1nicolasrg@gmail.com>
2022-03-03 12:50:36 -0500

    Styles from home section 6.

--- src/Pages/Home/SixSection/index.tsx
+++ src/Pages/Home/SixSection/index.tsx
@@ -1,49 +1,71 @@
+//* Import styles
+import {
+  Container,
+  ContainerSliders,
+  Slider,
+  Slide,
+  Users,
+  Controls,
+} from "./styles"
+
+//* Import assets
+import Star from "@assets/star.svg"
+import Arrow from "@assets/Next.svg"
+
 export const SixSection = () => {
   return (
-    <section>
+    <Container>
       <h4>Testimonials</h4>
       <h3>Our Happy Clients</h3>
-      <div>
-        <div>
-          <span>5.0</span>
-          <div>stars</div>
-          <p>
-            We tried several agencies before we found daCode. Has been a
-            pleasure to work with and will soon start the next project with our
-            second brand.
-          </p>
-        </div>
-        <div>
-          <div>
-            <ul>
-              <li>
-                <div />
-              </li>
-              <li>
-                <div />
-              </li>
-              <li>
-                <div />
-              </li>
-              <li>
-                <div />
-              </li>
-              <li>
-                <div />
-              </li>
-            </ul>
-
+      <Slider>
+        <ContainerSliders>
+          <Slide>
+            <span>5.0</span>
             <div>
-              <h4> Paulus Haverinen</h4>
-              <span>Owner, Ikirakenne Ltd</span>
+              <img src={Star} alt="" />
+              <img src={Star} alt="" />
+              <img src={Star} alt="" />
+              <img src={Star} alt="" />
+              <img src={Star} alt="" />
             </div>
-          </div>
-        </div>
-        <div>
-          <button>left</button>
-          <button>right</button>
-        </div>
-      </div>
-    </section>
+            <p>
+              We tried several agencies before we found daCode. Has been a
+              pleasure to work with and will soon start the next project with
+              our second brand.
+            </p>
+          </Slide>
+          <Controls>
+            <button>
+              <img src={Arrow} alt="" />
+            </button>
+            <button>
+              <img src={Arrow} alt="" />
+            </button>
+          </Controls>
+        </ContainerSliders>
+        <Users>
+          <ul>
+            <li>
+              <img />
+            </li>
+            <li>
+              <img />
+            </li>
+            <li>
+              <img />
+            </li>
+            <li>
+              <img />
+            </li>
+            <li>
+              <img />
+            </li>
+          </ul>
+
+          <h4> Paulus Haverinen</h4>
+          <span>Owner, Ikirakenne Ltd</span>
+        </Users>
+      </Slider>
+    </Container>
   )
 }
---
+++ src/Pages/Home/SixSection/styles.ts
@@ -0,0 +1,184 @@
+import styled from "styled-components"
+
+export const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  padding: 100px;
+  text-align: center;
+
+  h4 {
+    color: ${({ theme }) => theme.colors.secondary};
+  }
+`
+
+export const Slider = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
+`
+
+export const ContainerSliders = styled.div`
+  position: relative;
+  margin: auto;
+  margin-top: 30px;
+  max-width: 1024px;
+`
+
+export const Slide = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 80px;
+  box-shadow: 0px 25px 100px -50px #000;
+  border-radius: 24px;
+
+  span {
+    font-size: 38px;
+    font-weight: bold;
+  }
+`
+
+export const Users = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: auto;
+  max-width: 60%;
+  gap: 10px;
+
+  ul {
+    padding-bottom: 20px;
+    display: flex;
+    gap: 50px;
+
+    li {
+      position: relative;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 100px;
+      height: 100px;
+      border-radius: 100%;
+      transition: 0.2s all ease-in-out;
+      cursor: pointer;
+
+      &:hover {
+        transform: scale(1.1);
+      }
+
+      &:active {
+        transform: scale(0.9);
+      }
+
+      img {
+        background-color: royalBlue;
+        width: 100%;
+        height: 100%;
+        border-radius: 100%;
+        transition: 0.2s all ease-in;
+      }
+
+      &:nth-child(2),
+      &:nth-child(4) {
+        background-color: ${({ theme }) => theme.colors.grayLight};
+
+        &:after {
+          content: "";
+          position: absolute;
+          width: 100%;
+          height: 100%;
+          border-radius: 100%;
+          background: rgb(183 183 183 / 40%);
+        }
+
+        &:hover {
+          img {
+            width: 90%;
+            height: 90%;
+          }
+        }
+
+        img {
+          width: 80%;
+          height: 80%;
+        }
+      }
+
+      &:nth-child(1),
+      &:nth-child(5) {
+        background-color: ${({ theme }) => theme.colors.grayLight};
+
+        &:after {
+          content: "";
+          position: absolute;
+          width: 100%;
+          height: 100%;
+          border-radius: 100%;
+          background: rgb(183 183 183 / 60%);
+        }
+
+        &:hover {
+          img {
+            width: 80%;
+            height: 80%;
+          }
+        }
+
+        img {
+          width: 60%;
+          height: 60%;
+        }
+      }
+    }
+  }
+
+  h4 {
+    color: ${({ theme }) => theme.colors.primary};
+  }
+  span {
+    color: ${({ theme }) => theme.colors.secondary};
+  }
+`
+
+export const Controls = styled.div`
+  position: absolute;
+  top: 50%;
+  left: -8.5%;
+  display: flex;
+  justify-content: space-between;
+  margin: auto;
+  width: 1200px;
+
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 20px;
+    border-radius: 100%;
+    background-color: ${({ theme }) => theme.colors.whiteSecondary};
+    cursor: pointer;
+    transition: 0.2s all ease-in;
+    box-shadow: 0px 2px 8px -2px #000;
+
+    &:hover {
+      transform: scale(1.1);
+    }
+    &:nth-of-type(1) {
+      img {
+        transform: rotate(180deg);
+
+        &:hover {
+          transform: scale(1.1);
+          transform: rotate(180deg);
+        }
+      }
+    }
+
+    img {
+      &:hover {
+        transform: scale(1.1);
+      }
+    }
+  }
+`
