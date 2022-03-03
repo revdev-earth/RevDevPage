8f5e51bc03e2f24280dddaac6b72d72d5436bc1e
Nickothan <s1nicolasrg@gmail.com>
2022-03-03 10:53:34 -0500

    Layout content.

--- src/Pages/Home/SixSection/index.tsx
+++ src/Pages/Home/SixSection/index.tsx
@@ -1,3 +1,49 @@
 export const SixSection = () => {
-  return <div>Six Section</div>
+  return (
+    <section>
+      <h4>Testimonials</h4>
+      <h3>Our Happy Clients</h3>
+      <div>
+        <div>
+          <span>5.0</span>
+          <div>stars</div>
+          <p>
+            We tried several agencies before we found daCode. Has been a
+            pleasure to work with and will soon start the next project with our
+            second brand.
+          </p>
+        </div>
+        <div>
+          <div>
+            <ul>
+              <li>
+                <div />
+              </li>
+              <li>
+                <div />
+              </li>
+              <li>
+                <div />
+              </li>
+              <li>
+                <div />
+              </li>
+              <li>
+                <div />
+              </li>
+            </ul>
+
+            <div>
+              <h4> Paulus Haverinen</h4>
+              <span>Owner, Ikirakenne Ltd</span>
+            </div>
+          </div>
+        </div>
+        <div>
+          <button>left</button>
+          <button>right</button>
+        </div>
+      </div>
+    </section>
+  )
 }
