c00a2ebcbe48a8285b352916f2125ce258372388
Nickothan <s1nicolasrg@gmail.com>
2022-03-12 15:14:24 -0500

    Layout RPD-6-8-9.

--- src/Pages/Services/index.tsx
+++ src/Pages/Services/index.tsx
@@ -1,3 +1,50 @@
+import { Button } from "@components"
+
 export default function Services() {
-  return <div>Services</div>
+  return (
+    <>
+      <section>
+        <h3>What we can offer your SaaS Business</h3>
+        <p>
+          We have chosen to only collaborate and work with SaaS businesses, this
+          is because our expertise lies and we know that we can provide the most
+          value to our clients. Our team has worked with Nordic, European and
+          American tech unicorns.
+        </p>
+      </section>
+
+      <section>
+        <h3>Interested to work with us ?</h3>
+        <p>Send a line here get and update daily</p>
+        <Button primary>DaCode@example.com</Button>
+      </section>
+
+      <section>
+        <div>
+          <h3>Establishing online presence</h3>
+          <p>
+            Every SaaS website, regardless of niche, must do one thing
+            brilliantly, which is converting visitors into users. At first
+            glance, the site should encourage and guide visitors in a smooth way
+            towards call-to-actions. This goes hand in hand with a responsive
+            design, meaning it needs to be apt for different devices. We use a
+            data-driven approach to measure user response when developing the
+            site. This method usually makes the site quicker to launch, is more
+            cost-effective and more successful in the long run. The pages need
+            to be search engine optimized (SEO) because it lays the foundation
+            for the technical quality, which in turn determines how high it will
+            rank among search results. We also make sure that your website is
+            indexed properly. We audit your audience and get to know your target
+            market to be able to speak to them through the website in the best
+            possible way. By finding out their consumer behavior we can refine
+            the website approach.
+          </p>
+        </div>
+        <div>
+          <h4>Schedule a free session</h4>
+          <img />
+        </div>
+      </section>
+    </>
+  )
 }
