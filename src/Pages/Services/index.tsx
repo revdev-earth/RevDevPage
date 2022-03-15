92002caa652f7d56e13f96aa1a69633e60ee99ec
Nickothan <s1nicolasrg@gmail.com>
2022-03-15 13:00:23 -0500

    Make, layout and styling seconde section from services.

--- src/Pages/Services/index.tsx
+++ src/Pages/Services/index.tsx
@@ -7,6 +7,7 @@ import { Button } from "@components"
 
 //* Import sections
 import { FirstSection } from "./components_/FirstSection"
+import { SecondSection } from "./components_/SecondSection"
 import { ThirdSection } from "./components_/ThirdSection"
 import { FourthSection } from "./components_/FourthSection"
 
@@ -14,6 +15,7 @@ export default function Services() {
   return (
     <>
       <FirstSection />
+      <SecondSection />
       <ThirdSection />
       <FourthSection />
     </>
