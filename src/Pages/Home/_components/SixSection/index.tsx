c1a7ae65862d66feac1c0a20bfc3dd9da65c6893
Revi <oliiv@outlook.com>
2022-04-12 13:09:19 -0300

    try

--- src/Pages/Home/_components/SixSection/index.tsx
+++ src/Pages/Home/_components/SixSection/index.tsx
@@ -13,7 +13,7 @@ import {
 //* Import assets
 import Star from "@assets/star.svg"
 import Circles from "@assets/circles.svg"
-import Img3 from "@assets/home/image-testimonal-three.png"
+import Img3 from "@assets/home/testimonials/image-testimonal-three.png"
 import Img2 from "@assets/home/testimonials/image-testimonal-two.png"
 import Img1 from "@assets/home/testimonials/image-testimonial-one.png"
 
