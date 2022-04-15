c7f9820b995c0a91998fdf6160ddc992591a672c
Nickothan <s1nicolasrg@gmail.com>
2022-04-15 09:26:10 -0500

    Update vector-black.

--- src/components/fewUses/Footer/index.tsx
+++ src/components/fewUses/Footer/index.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 //* Import styles
 import { Top, Container, Bottom, Content, Images } from "./styles"
 //* Import assets
-import Arrow from "@assets/vector-black.svg"
+import Arrow from "@assets/vector-black.svg-"
 import Img1 from "@assets/vector1.svg"
 import Img2 from "@assets/vector2.svg"
 
